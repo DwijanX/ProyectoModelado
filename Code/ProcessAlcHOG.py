f3a25c62f9be45c11cbfbda94b5ecbe133aa1307
E-Doggo <94150885+E-Doggo@users.noreply.github.com>
2022-06-15 17:19:16 -0400

    Cambio a funciones HOG

---
+++ Code/ProcessAlcHOG.py
@@ -0,0 +1,147 @@
+#%%
+import cv2
+from cv2 import INTER_AREA
+import matplotlib.pyplot as pl
+import os
+import numpy
+from PIL import Image
+import h5py
+from skimage.feature import hog
+# assign directory
+
+
+def WhitePatch(Img):
+    Img_mean = (Img*1.0 / Img.mean(axis=(0,1)))
+    return Img_mean
+
+def ProcessImg(PilImg,ApplyWhitePatch,HOG=False):
+    PilImg = PilImg.resize((128, 128), Image.ANTIALIAS)
+    npImg = numpy.array(PilImg) 
+    if HOG:
+        fd, npImg = hog(npImg, orientations=9, pixels_per_cell=(8, 8),
+                	cells_per_block=(2, 2), visualize=True, channel_axis=-1)
+    if ApplyWhitePatch:
+        npImg=WhitePatch(npImg)
+    return npImg
+
+def getArrayOfImagesFromDir(dir,label,Rotate=False,ApplyWhitePatch=False,HOG=False):
+    count=0
+    Images=[]
+    for filename in os.listdir(dir):
+        f = os.path.join(dir, filename)
+        if os.path.isfile(f):
+            img = Image.open(f)
+            if Rotate:
+                img = img.rotate(-90, Image.NEAREST, expand = 1)
+            
+            img=ProcessImg(img,ApplyWhitePatch,HOG)
+            """if img.shape!=(128,128,3):
+                print(f)
+                print(img.shape)"""
+            #pl.imshow(img)
+            #pl.show()
+            count+=1
+            Images.append(img)
+    y=numpy.ones(count)
+    y=label*y
+    ImagesNp=numpy.array(Images)
+    print(ImagesNp.shape)
+    return ImagesNp,y
+
+directory1 = '../Images/Alcohol/Beer'
+directory2 = '../Images/Alcohol/botellas'
+
+
+directory5 = '../Images/Soda/M.Beer'
+directory6 = '../Images/Soda/CanadaDry/CanadaDry'
+directory9 = '../Images/Soda/P.Orig'
+directory7 = '../Images/Soda/P.cherry'
+
+directory10 = '../Images/Soda/M.Diet'
+
+directory3 = '../Images/Alcohol/cans'
+directory8 = '../Images/Alcohol/cansRodri'
+
+#0 soda
+#1 tarro de cerveza
+#2 botella de alcohol
+#3 lata de alcohol
+#4 copas de vino
+X1,Y1=getArrayOfImagesFromDir(directory3,1,True,HOG=True)
+X2,Y2=getArrayOfImagesFromDir(directory8,1,HOG=True)
+X=numpy.append(X1,X2,axis=0)
+Y=numpy.append(Y1,Y2,axis=0)
+
+X3,Y3=getArrayOfImagesFromDir(directory5,0,HOG=True)
+X=numpy.append(X,X3,axis=0)
+Y=numpy.append(Y,Y3,axis=0)
+X4,Y4=getArrayOfImagesFromDir(directory6,0,HOG=True)
+X=numpy.append(X,X4,axis=0)
+Y=numpy.append(Y,Y4,axis=0)
+X4,Y4=getArrayOfImagesFromDir(directory9,0,HOG=True)
+X=numpy.append(X,X4,axis=0)
+Y=numpy.append(Y,Y4,axis=0)
+X4,Y4=getArrayOfImagesFromDir(directory7,0,HOG=True)
+X=numpy.append(X,X4,axis=0)
+Y=numpy.append(Y,Y4,axis=0)
+X4,Y4=getArrayOfImagesFromDir(directory10,0,HOG=True)
+X=numpy.append(X,X4,axis=0)
+Y=numpy.append(Y,Y4,axis=0)
+
+
+"""
+X4,Y4=getArrayOfImagesFromDir(directory9,0,True)
+X=numpy.append(X,X4,axis=0)
+Y=numpy.append(Y,Y4,axis=0)
+
+
+X5,Y5=getArrayOfImagesFromDir(directory3,1,ApplyWhitePatch=False)
+X=numpy.append(X,X5,axis=0)
+Y=numpy.append(Y,Y5,axis=0)"""
+"""
+X6,Y6=getArrayOfImagesFromDir(directory6,0,ApplyWhitePatch=False)
+X=numpy.append(X,X6,axis=0)
+Y=numpy.append(Y,Y6,axis=0)
+X7,Y7=getArrayOfImagesFromDir(directory7,0,ApplyWhitePatch=False)
+X=numpy.append(X,X7,axis=0)
+Y=numpy.append(Y,Y7,axis=0)
+
+X8,Y8=getArrayOfImagesFromDir(directory8,4)
+X=numpy.append(X,X8,axis=0)
+Y=numpy.append(Y,Y8,axis=0)
+
+X9,Y9=getArrayOfImagesFromDir(directory9,0,ApplyWhitePatch=False)
+X=numpy.append(X,X9,axis=0)
+Y=numpy.append(Y,Y9,axis=0)
+"""
+
+
+print(X.shape)
+print(Y.shape)
+#for x in X:
+#    pl.imshow(x)
+#    pl.show()
+#print(X.shape)
+#print(Y.shape)
+
+t=h5py.File('../Images/DatasetAlcoholHOG.h5','w')
+t.create_dataset('Y_TrainSet',data=Y)
+t.create_dataset('X_TrainSet',data=X)
+t.close()
+"""
+Images=numpy.array(Images)
+
+
+ImagesResized=[]
+for img in Images:
+    aux=resizeImg(img)
+    ImagesResized.append(aux)
+"""
+
+
+
+""" 
+imagen=cv2.imread("../Images/Alcohol/8c43d837650c46528921251de55e24f4.jpg")
+
+"""  
+
---
+++ Code/TestCameraPytorchHOG.py
@@ -0,0 +1,64 @@
+#%%
+import numpy
+import joblib
+from sklearn.svm import SVC
+import matplotlib.pyplot as pl
+import h5py
+import cv2
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+from PIL import Image
+import torch.nn.functional as F
+import torch
+from skimage.feature import hog
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+#model_ft = torch.load("Code\mi_modeloDeAlcohol.pt")
+model_ft = torch.load("Code\mi_modeloDeAlcoholHOG.pt")
+def ProcessImg(Img):
+        GrayImage=cv2.cvtColor(Img,cv2.COLOR_BGR2GRAY)
+        thresh=150
+        ret,thresh_img = cv2.threshold(GrayImage, thresh, 255, cv2.THRESH_BINARY)
+        #pl.imshow(thresh_img)
+        #pl.show()
+        grupos,_=cv2.findContours(thresh_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        ventanas= [cv2.boundingRect(g) for g in grupos]
+        fd, hogImg = hog(Img, orientations=9, pixels_per_cell=(8, 8),
+                    cells_per_block=(2, 2), visualize=True, channel_axis=-1)
+        for bots in ventanas:
+            (x, y, w, h) = bots
+            espacio=int(bots[3]*1.6)
+            p1=int((bots[1]+bots[3]//2))-espacio//2
+            p2=int((bots[0]+bots[2]//2))-espacio//2
+            JustBottle = hogImg[y:y + h+espacio,x:x+w+espacio]
+            
+            if  w>30 and h>30:
+                pilImg = Image.fromarray(JustBottle)
+                JustBottle = pilImg.resize((128,128))
+                cv2.rectangle(Img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                bottle = transform(JustBottle)
+                bottle.unsqueeze_(dim=0)
+                bottle = Variable(bottle)
+                bottle = bottle.view(bottle.shape[0], -1)
+                ansvec = F.alpha_dropout(model_ft(bottle))
+                ans=ansvec.argmax().item()
+                cv2.putText(Img,str(ans),(x,y+50),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0))
+        return Img
+#clasificador=joblib.load('Code\ParametrosProyecto')
+#clasificador=joblib.load('./ParametrosProyecto')
+
+
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+    
+    frame=ProcessImg(frame)
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(49) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
